75686f670e263cb2c9fb8cb0ac1be035c9051063
tomas <72792707+tomas-land@users.noreply.github.com>
2023-05-11 00:00:49 +0300

    minor changes

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -1,24 +1,78 @@
-import sendgrid from "@sendgrid/mail";
+import nodemailer from "nodemailer";
 
-sendgrid.setApiKey(process.env.SENDGRID_API_KEY || "");
-export async function GET(request) {
+const email = process.env.EMAIL;
+const pass = process.env.EMAIL_PASS;
+
+export async function POST(request) {
   const { name, email, message } = await request.json();
-  const msg = {
-    to: "tomaslanda1989@gmail.com",
-    from: "tomaslanda1989@gmail.com",
-    subject: "Tomas sent you a message from Toams",
-    text: "message",
+
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: email,
+      pass,
+    },
+  });
+
+  const mailOptions = {
+    from: email,
+    to: email,
   };
+
   try {
-    await sendgrid.send(msg);
-    // console.log('email sent');
-    // return new Response({ status: 200 });
+    await transporter.sendMail({
+      ...mailOptions,
+      subject: `Message from ${name}`,
+      text: message,
+    });
+    return new Response({ status: 200 });
   } catch (error) {
     console.log(error);
-    return new Response({ status: 500, message: "api error" });
+    return new Response({ status: 400, message: error.message });
   }
-  return new Response({ status: 200 });
+  return new Response({ status: 400, message: "bad request" });
 }
+//   // send mail with defined transport object
+//   let info = await transporter.sendMail({
+//     from: '"Fred Foo 👻" <foo@example.com>', // sender address
+//     to: "bar@example.com, baz@example.com", // list of receivers
+//     subject: "Hello ✔", // Subject line
+//     text: "Hello world?", // plain text body
+//     html: "<b>Hello world?</b>", // html body
+//   });
+
+//   console.log("Message sent: %s", info.messageId);
+//   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+//   // Preview only available when sending through an Ethereal account
+//   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+//   // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+// }
+
+// main().catch(console.error);
+
+// import sendgrid from "@sendgrid/mail";
+
+// sendgrid.setApiKey(process.env.SENDGRID_API_KEY || "");
+// export async function POST(request) {
+//   // const { name, email, message } = await request.json();
+//   return Response.json({msg: "hello"});
+// }
+//   const msg = {
+//     to: "tomaslanda1989@gmail.com",
+//    from: "tomaslanda1989@gmail.com",
+//    subject: "Tomas sent you a message from Toams",
+//    text: "message",
+// }
+//   try {
+//     await sendgrid.send(msg);
+//     // console.log('email sent');
+//     // return new Response({ status: 200 });
+//   } catch (error) {
+//     console.log(error);
+//     return new Response({ status: 500 ,message: "api error"});
+//   }
+//   return new Response({ status: 200 });
 
 // const connectedStrings = name+email+message;
 // console.log(connectedStrings);
