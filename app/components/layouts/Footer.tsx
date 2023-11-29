20b9e0ace3e4a922a2ce341b6ea9ad0f3b6f325a
Tomas Landa <tomaslanda1989@gmail.com>
2023-11-29 14:11:36 +0200

    update link to older projects

--- app/components/layouts/Footer.tsx
+++ app/components/layouts/Footer.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const Footer = () => {
   return (
     <footer className={s.footer}>
-      <Link className={s.link_to_old_version} href="https://portfolio-v-1-black.vercel.app/" target="_blank" rel="noopener noreferrer">v1.0</Link>
+      <Link className={s.link_to_old_version} href="https://portfolio-v-1-black.vercel.app/" target="_blank" rel="noopener noreferrer">v1.0 (older projects)</Link>
       <h4 className={s.copyright}>© 2023 Created By Me</h4>
     </footer>
   )
